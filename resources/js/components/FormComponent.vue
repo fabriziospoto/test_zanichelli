d1eb67d9fb6426cf4b703ddd0f5e130030a8df87
fabriziospoto <66363231+fabriziospoto@users.noreply.github.com>
2021-11-27 12:50:16 +0100

    Added Vue, Bootstrap and component
    Added vue, bootstrap and component

---
+++ resources/js/components/FormComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <label for="exampleFormControlInput1">Nome</label>
+        <input class="form-control" type="text" placeholder="Mario">
+        <label for="exampleFormControlInput1">Cognome</label>
+        <input class="form-control" type="text" placeholder="Rossi">
+        <label for="exampleFormControlInput1">E-Mail</label>
+        <input class="form-control" type="text" placeholder="mario.rossi@mail.com">
+    </div>
+</template>
+
+<script>
+export default ({
+    
+})
+</script>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,13 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+
+    <title>Test Zanichelli</title>
+
+    <script src="{{ asset('/js/app.js') }}" defer></script>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet" >
+
 </head>
 <body>
-    <div>
-        <p>
-            Prova
-        </p>
+    <div id="app" class="container d-flex justify-content-center">
+        <form-component></form-component>
     </div>
 </body>
 </html>
